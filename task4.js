c033451f2aca1dd10e4591a051378f7cf5df2c6f
PyTrainee <eternal@disroot.org>
2024-03-09 16:27:38 +0200

    Initial commit

---
+++ task4.js
@@ -0,0 +1,20 @@
+// На HTML-сторінці є 6 чекбоксів. 
+// Напишіть скріпт, який після того, 
+// як користувач позначив будь-які 3 чекбокси,
+// всі чекбокси робить неактивними.
+
+// Завдання виділене вставкою Task4
+let selectedCount = 0;
+
+
+$('input[type="checkbox"]').on('change', function() {
+    if ($(this).is(':checked')) {        
+        selectedCount++;
+    } else {        
+        selectedCount--;
+    }
+    if (selectedCount >= 3) {
+        $('input[type="checkbox"]').prop('disabled', true);
+    }
+});
+ 
